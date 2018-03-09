1ca54df591c01c31ffb979c97a3689aed75045f7
Dj <rodriguezdanilo88@gmail.com>
2018-03-09 11:21:46 -0800

    all systems go ;)

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,17 +3,22 @@ import Todo from './Todo';
 
 export default function Todos(props) {
     return (
-        <ul className='list-group mr-4'>
-         {
-            props.todoList.length < 1 ? 
-                (<li className='list-group-item list-group-item-primary'>
-                    <div>
-                        <span className='todoDescription font-weight-bold'>Welcome to Very Simple Todo App!</span>
-                        <p>Get started now by adding a new todo on the left.</p>
-                    </div>
-                </li>)
-                : props.todoList.map((todoItem, index) => <Todo todoItem={todoItem} index={index} key={todoItem.description + index} bigStateEdit={props.bigStateEdit} bigStateDeleter={props.bigStateDeleter}/>)
-         }
-        </ul>
+        <div className='card'>
+            <div className='card-header'>
+                View Todo
+            </div>
+            <ul className='list-group'>
+            {
+                props.todoList.length < 1 ? 
+                    (<li className='list-group-item list-group-item-primary'>
+                        <div>
+                            <span className='todoDescription font-weight-bold'>Welcome to Very Simple Todo App!</span>
+                            <p>Get started now by adding a new todo on the left.</p>
+                        </div>
+                    </li>)
+                    : props.todoList.map((todoItem, index) => <Todo todoItem={todoItem} index={index} key={todoItem.description + index} bigStateEdit={props.bigStateEdit} bigStateDeleter={props.bigStateDeleter}/>)
+            }
+            </ul>
+        </div>
     )
 }
